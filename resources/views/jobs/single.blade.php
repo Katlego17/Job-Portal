9dc2686dc26f14a7d1aa158740d348b257ac8423
KnKSoftwareSolutions <KnKSoftwareSolutions.SA@gmail.com>
2024-05-21 23:21:05 +0200

    Added profile page

--- resources/views/jobs/single.blade.php
+++ resources/views/jobs/single.blade.php
@@ -380,5 +380,16 @@
             </div>
         </div>
     </div>
+    <!-- Core Vendors JS -->
+    <script src="assets/js/vendors.min.js"></script>
+
+    <!-- page js -->
+    <script src="assets/vendors/chartjs/Chart.min.js"></script>
+    <script src="assets/js/pages/dashboard-default.js"></script>
+
+    <!-- Core JS -->
+    <script src="assets/js/app.min.js"></script>
+    <!-- page js -->
+    <script src="assets/vendors/select2/select2.min.js"></script>
 </body>
 </html>

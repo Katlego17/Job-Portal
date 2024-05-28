3d4ffe989698a1fff3925cc4462816a29d50a125
Kat(Katlego) Mashishi <61347522+Katlego17@users.noreply.github.com>
2024-05-28 10:31:37 +0200

    Update where-i-work.blade.php

--- resources/views/where-i-work.blade.php
+++ resources/views/where-i-work.blade.php
@@ -15,7 +15,7 @@
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg bg-white sticky-top">
         <div class="container">
-            <a class="navbar-brand" href="#">
+            <a class="navbar-brand" href="/">
                 <img src="./assets/img/logo.png" alt="" class="logo-img">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"

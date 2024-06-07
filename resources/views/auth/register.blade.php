4364f11208439bf92e8952897c17cdd421c2d53b
feezy101 <159916627+feezy101@users.noreply.github.com>
2024-06-07 18:39:48 +0200

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -74,14 +74,7 @@
             </div>
             <div class="d-none d-md-flex p-h-40 justify-content-between">
                 <span class=""></span>
-                <ul class="list-inline">
-                    <li class="list-inline-item">
-                        <a class="text-dark text-link" href="">Legal</a>
-                    </li>
-                    <li class="list-inline-item">
-                        <a class="text-dark text-link" href="">Privacy</a>
-                    </li>
-                </ul>
+                
             </div>
         </div>
     </div>
